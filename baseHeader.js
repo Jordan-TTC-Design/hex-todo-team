de02873bf0b3aac673bde33988bb165c5e7e5cb8
Hank Hsiao <cgh20xx@gmail.com>
2022-04-03 15:31:00 +0800

    update baseHeader.js 建立更多驗證機制 title 需為字串

--- baseHeader.js
+++ baseHeader.js
@@ -1,33 +1,35 @@
-const { v4:uuidv4 } =require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
-const headers={
-    //'Content-Type' : 'text/lain',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
-    'Content-Type': 'application/json'
+const headers = {
+  //'Content-Type' : 'text/lain',
+  'Access-Control-Allow-Headers':
+    'Content-Type, Authorization, Content-Length, X-Requested-With',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+  'Content-Type': 'application/json',
 };
 
-function changtodos(req,todos,body){
-    const title =JSON.parse(body).title;
-    if(req.method==='POST'){
-        const todo={
-            'title': title,
-            'id': uuidv4()
-        };
-        todos.push(todo);
-        return true;
+function changtodos(req, todos, body) {
+  const title = JSON.parse(body).title;
+  if (typeof title !== 'string') return false;
+  if (req.method === 'POST') {
+    const todo = {
+      title: title,
+      id: uuidv4(),
+    };
+    todos.push(todo);
+    return true;
+  } else {
+    const id = req.url.split('/').pop();
+    const index = todos.findIndex((e) => e.id == id);
+    if (title !== undefined && index !== -1) {
+      todos[index].title = title;
+      return true;
     } else {
-        const id = req.url.split('/').pop();
-        const index = todos.findIndex(e=>e.id==id);
-        if(title!== undefined && index !==-1){
-            todos[index].title=title;
-            return true;
-        } else {
-            return false;
-        }
+      return false;
     }
-    return false;
+  }
+  return false;
 }
 
-module.exports={headers,changtodos};
+module.exports = { headers, changtodos };
